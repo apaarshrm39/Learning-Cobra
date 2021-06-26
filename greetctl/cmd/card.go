6efc282d1adb634042da75d0fbb2baa40826b79b
Apaar Sharma <Apaar.Sharma@appliedis.com>
2021-06-26 18:32:56 +0530

    added create card and get card

--- greetctl/cmd/card.go
+++ greetctl/cmd/card.go
@@ -17,27 +17,73 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 // cardCmd represents the card command
 var cardCmd = &cobra.Command{
-	Use:   "card",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "card <name>",
+	Short: "This command creates fun greetings",
+	Long: `For example:
+	greetctl create card eva -n="Eva Green" -o=thanksgiving -l=fr `,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("card called")
+		fmt.Println("Here are the arguments of card command : " + strings.Join(args, ","))
+
+		name, _ := cmd.Flags().GetString("name")
+		language, _ := cmd.Flags().GetString("language")
+		fmt.Println("value of the flag name :" + name)
+		fmt.Println("value of the flag language :" + language)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(cardCmd)
+	createCmd.AddCommand(cardCmd)
+	cardCmd.PersistentFlags().StringP("occasion", "o", "", "Possible values: newyear, thanksgiving, birthday")
+	cardCmd.PersistentFlags().StringP("language", "l", "en", "Possible values: en, fr")
+	cardCmd.PersistentFlags().StringP("name", "n", "", "Name of the user to whom you want to greet")
+	cardCmd.MarkPersistentFlagRequired("name")
+	cardCmd.MarkPersistentFlagRequired("occasion")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cardCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cardCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+var card2Cmd = &cobra.Command{
+	Use:   "card <name>",
+	Short: "This command creates fun greetings",
+	Long: `For example:
+	greetctl create card eva -n="Eva Green" -o=thanksgiving -l=fr `,
+	Run: func(cmd *cobra.Command, args []string) {
+		o, _ := cmd.Flags().GetString("ocassion")
+		n, _ := cmd.Flags().GetString("name")
+		switch o {
+		case "birthday":
+			fmt.Println("Happy Birthday !", n)
+		case "newyear":
+			fmt.Println("Happy newyear !", n)
+		default:
+			fmt.Println("Happy Thanksgiving")
+		}
+	},
+}
+
+func init() {
+	getCmd.AddCommand(card2Cmd)
+	card2Cmd.PersistentFlags().StringP("occasion", "o", "", "Possible values: newyear, thanksgiving, birthday")
+	card2Cmd.PersistentFlags().StringP("language", "l", "en", "Possible values: en, fr")
+	card2Cmd.PersistentFlags().StringP("name", "n", "", "Name of the user to whom you want to greet")
+	card2Cmd.MarkPersistentFlagRequired("name")
+	card2Cmd.MarkPersistentFlagRequired("occasion")
 
 	// Here you will define your flags and configuration settings.
 
--- greetctl/steps.md
+++ greetctl/steps.md
@@ -1,8 +1,49 @@
 1. go mod init greetctl (Create a go module for CLI.)
+   
 2. go get -u github.com/spf13/cobra/cobra (Get Cobra library)
-3. cobra init --pkg-name greetctl (Create a bare minimum skeleton)
-4. go get github.com/mitchellh/go-homedir@v1.1.0 (library required to get home-dir without cgo)
-5. run the command go run main.go to test.
-6. To add a new command do cobra add create. New go file will be created under ./cmd/create.go
-7. Do cobra add card, By default, both create and card commands will be added to the top-level command (root.go). But we want to add card (card.go) sub-command to create (create.go) command instead of top-level command (root.go) so lets edit /cmd/card.go. Add cardCmd in createCmd instead of rootCmd. </br>
-![initial](./img/initial-card.pngimage.png)
+ 
+4. cobra init --pkg-name greetctl (Create a bare minimum skeleton)
+   
+5. go get github.com/mitchellh/go-homedir@v1.1.0 (library required to get home-dir without cgo)
+   
+6. run the command go run main.go to test.
+   
+7.  To add a new command do cobra add create. New go file will be created under ./cmd/create.go
+   
+8.  Do cobra add card, By default, both create and card commands will be added to the top-level command (root.go). But we want to add card (card.go) sub-command to create (create.go) command instead of top-level command (root.go) so lets edit /cmd/card.go. Add cardCmd in createCmd instead of rootCmd. </br>
+![initial](./img/initial-card.png) </br>
+![initial](./img/later-card.jpg) </br>
+
+8. Update cardCmd as per given specification <br>
+![initial](./img/card.jpg) </br>
+
+## Reading Arguments 
+
+1. Try "go run main.go create card my-card" and add to card.go as picture given Below:
+![initial](./img/args.jpg) </br>
+
+## Adding Flags
+
+So far we know, how to create commands and read arguments. In this section, we will create few flags with descriptions and read them in the program.
+
+1. Flags can be read in the card.go with 
+```
+
+Run: func(cmd *cobra.Command, args []string) {
+  name, _ := cmd.Flags().GetString("name")
+  language, _ := cmd.Flags().GetString("language")
+  fmt.Println("value of the flag name :" + name)
+  fmt.Println("value of the flag language :" + language)
+},
+
+``` 
+
+## Install CTL
+
+Once you are done with all the changes you should create binary using following command.
+
+```
+
+go install greetctl
+
+```
